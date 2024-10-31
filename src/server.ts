7cb1ebf7d92db01befbedadb8062d29d05b682aa
Niladri <niladrisengit12@gmail.com>
2024-11-01 02:51:25 +0530

    express-server-setup

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
 import { Config } from './config';
+import app from './app';
+const startServer = () => {
+    try {
+        app.listen(Config.PORT, () => console.log('server is running'));
+        console.log(`Server started on port ${Config.PORT}`);
+    } catch (err) {
+        console.error(err);
+    }
+};
 
-console.log(Config.PORT);
+startServer();
