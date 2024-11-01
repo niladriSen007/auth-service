49648206f39c68660d9f696e81b7b7e1eb5cc6da
Niladri <niladrisengit12@gmail.com>
2024-11-01 10:10:02 +0530

    add errorhandler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,30 @@
-import express from 'express';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express, { NextFunction, Request, Response } from 'express';
+import { logger } from './config/logger';
+import createHttpError, { HttpError } from 'http-errors';
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    const error = createHttpError(500, 'Internal Server Error');
+    throw error;
+    /*  next(error);  */ //if the function is async then we will have to use this in express v4.0 but in v.5.0 we can use throw error
+});
+
+//Global error handler
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err?.message);
+    const status = err?.status || 500;
+
+    res.status(status).json({
+        errors: [
+            {
+                type: err?.name,
+                message: err?.message,
+                path: '',
+                location: '',
+            },
+        ],
+    });
 });
 
 export default app;
